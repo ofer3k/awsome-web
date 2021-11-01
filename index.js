943d168ae1df224f941f71d55597a031b33d3265
ofer3k <ofer3klein@gmail.com>
2021-11-01 16:47:05 +0200

    third commit

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 8000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
+// app.get("/", (req, res) => {
+//   res.send("hello world");
+// });
+
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.sendFile("./public/index.html", { root: __dirname });
 });
+
 app.get("/data", (req, res) => {
   res.send({ name: "ofer" });
 });
-app.get("dataJson", (req, res) => {
+app.get("/dataJson", (req, res) => {
   res.send(JSON.stringify({ name: "ofer" }));
 });
 app.listen(port, () => {
