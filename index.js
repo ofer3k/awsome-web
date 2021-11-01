e99c2473d677155868bdda5488a9b9e0adc30da8
ofer3k <ofer3klein@gmail.com>
2021-11-01 16:36:34 +0200

    seconde commit

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 8000;
+
 app.get("/", (req, res) => {
   res.send("hello world");
+});
+app.get("/data", (req, res) => {
+  res.send({ name: "ofer" });
+});
+app.get("dataJson", (req, res) => {
+  res.send(JSON.stringify({ name: "ofer" }));
 });
 app.listen(port, () => {
   console.log(`Running on ${port}`);
